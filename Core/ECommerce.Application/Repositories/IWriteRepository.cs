e6043a80db6503c12d1d0365ddca3bb119002531
murat uçar <murat_ua@outlook.com>
2022-03-08 01:01:04 +0300

    IRepository is added

---
+++ Core/ECommerce.Application/Repositories/IWriteRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Application.Repositories
+{
+    public interface IWriteRepository<T> : IRepository<T> where T : class
+    {
+        Task<bool> AddAsync(T model);
+        Task<bool> AddAsync(List<T> model);
+        Task<bool> Remove(T model);
+        Task<bool> Remove(string Id);
+        Task<bool> UpdateAsync(T model);
+
+    }
+}
